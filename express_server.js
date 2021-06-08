e347b2b5c9c9834e1bcbe8719fd52dd0e804727e
alexeyosipov88 <aoosipov88@gmail.com>
2021-06-08 18:14:50 +0000

    added delete URL

--- express_server.js
+++ express_server.js
@@ -22,10 +22,6 @@ app.post("/urls", (req, res) => {
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
-app.post("/urls/:shortURL/delete", (req, res) => {
-  
-  res.redirect(`/urls/${shortURL}`);
-});
 
 
 app.get("/", (req, res) => {
@@ -45,10 +41,16 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
   res.render("urls_show", templateVars);
 });
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
