2e3ab2e2efa46c41f8a0d2dc6127c35f995a539c
alexeyosipov88 <aoosipov88@gmail.com>
2021-06-10 16:17:55 +0000

    fixed redirection to longURL bug

--- express_server.js
+++ express_server.js
@@ -133,7 +133,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });   
 app.post("/logout", (req, res) => {
