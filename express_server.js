640affb86332ed50738ec86a3d7abf673cef3f23
alexeyosipov88 <aoosipov88@gmail.com>
2021-06-10 06:29:14 +0000

    added user_id cookie

--- express_server.js
+++ express_server.js
@@ -51,20 +51,23 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user_id: req.cookies["user_id"],
+    user: users[req.cookies["user_id"]]
   }
   res.render("urls_index", templateVars);
 });
 app.get("/urls/new", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user_id: req.cookies["user_id"],
+    user: users[req.cookies["user_id"]]
   }
   res.render("urls_new", templateVars);
 });
 app.get("/register", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"]
+    user_id: req.cookies["user_id"],
+    user: users[req.cookies["user_id"]]
   }
   res.render("urls_register", templateVars);
 });
@@ -83,7 +86,8 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies["username"]
+    user_id: req.cookies["username"],
+    user: users[req.cookies["user_id"]]
   }
   res.render("urls_show", templateVars);
 });
@@ -102,7 +106,7 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 app.post("/logout", (req, res) => {
-  res.clearCookie('username')
+  res.clearCookie('user_id')
   res.redirect("/urls");
 });
 
